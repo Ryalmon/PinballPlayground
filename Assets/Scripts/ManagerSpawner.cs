b20bce670d2467dd32085f2d72584d38f7d9bf75
rswansonbradley <raswanson@mail.bradley.edu>
2023-11-03 15:58:25 -0500

    SetUpFolders

---
+++ Assets/Scripts/ManagerSpawner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerSpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _allManagers;
+
+    private void Awake()
+    {
+        foreach (GameObject m in _allManagers)
+        {
+            Instantiate(m, transform.position, transform.rotation);
+        }
+    }
+}
