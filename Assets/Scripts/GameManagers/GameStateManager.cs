506bfc529a5ebead7d1b559c3ebc59dd7055d02a
rswansonbradley <raswanson@mail.bradley.edu>
2023-11-09 16:03:06 -0600

    BallAndGameStateManager

--- Assets/Scripts/GameManagers/GameStateManager.cs
+++ Assets/Scripts/GameManagers/GameStateManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour
 {
+    [SerializeField] private GameObject BallPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,15 @@ public class GameStateManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Testing Spawn Ball, will remove later
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnBall();
+        }
+    }
+
+    public void SpawnBall()
+    {
+        Instantiate(BallPrefab, Vector2.zero, Quaternion.identity);
     }
 }
