7af394defce80567f4b29aa8e0059548fbbf5c2e
rswansonbradley <raswanson@mail.bradley.edu>
2023-11-10 15:49:44 -0600

    GameStateEnum

--- Assets/Scripts/GameManagers/GameStateManager.cs
+++ Assets/Scripts/GameManagers/GameStateManager.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class GameStateManager : MonoBehaviour
 {
     [SerializeField] private GameObject BallPrefab;
+
+    internal GamePlayState GPS;
+
+    public enum GamePlayState { 
+        Intro,
+        Play,
+        End
+    };
     // Start is called before the first frame update
     void Start()
     {
