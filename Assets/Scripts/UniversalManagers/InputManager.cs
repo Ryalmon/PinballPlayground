2008099b766ea9e9823d445f73f84a248d8af1a5
rswansonbradley <raswanson@mail.bradley.edu>
2023-11-10 19:10:25 -0600

    SetupTouchInput

--- Assets/Scripts/UniversalManagers/InputManager.cs
+++ Assets/Scripts/UniversalManagers/InputManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     public static InputManager M_Instance;
+    public TouchInput TI;
+
     void Awake()
     {
         if (EstablishSingleton())
@@ -25,10 +28,16 @@ public class InputManager : MonoBehaviour
         return false;
     }
 
-    void SetupInput()
+    /*void SetupInput()
     {
-        
+        TI = new TouchInput();
+        TI.Controls.TestTouch.started += ctx => StartTouch(ctx);
     }
+
+    void StartTouch(InputAction.CallbackContext context)
+    {
+        Debug.Log(TI.Controls.TestTouch.ReadValue<Vector2>());
+    }*/
 
     // Update is called once per frame
     void Update()
