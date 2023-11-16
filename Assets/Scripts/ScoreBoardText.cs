98691977e036079d7679c3c3cef5e5e6fc3c5142
rswansonbradley <raswanson@mail.bradley.edu>
2023-11-16 17:49:08 -0600

    ScoreBoardText

---
+++ Assets/Scripts/ScoreBoardText.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreBoardText : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] int _scorePosition;
+    [Space]
+    [Header("References")]
+    [SerializeField] TMP_Text _nameText;
+    [SerializeField] TMP_Text _scoreText;
+
+    private void Awake()
+    {
+        //ChangeText();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            ChangeText();
+        }
+    }
+
+    private void ChangeText()
+    {
+        Debug.Log(SaveManager.M_Instance);
+        _nameText.text = SaveManager.M_Instance.ReturnPlayerName(_scorePosition);
+        _scoreText.text = SaveManager.M_Instance.ReturnPlayerScore(_scorePosition).ToString();
+    }
+}
