050d35931cf052ca0c3a72a0159deb1b1af8ebf1
rswansonbradley <raswanson@mail.bradley.edu>
2024-02-01 13:22:35 -0600

    ManagerChanges

---
+++ Assets/Scripts/UniversalManagers/UniversalManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _allManagers;
+    public InputManager Input;
+    public SaveManager Save;
+    public SceneLoadingManager Scene;
+    public VFXManager VFX;
+
+    public static UniversalManager Instance;
+
+
+    private void Awake()
+    {
+        EstablishSingleton();
+        SpawnManagers();
+        AssignManagers();
+    }
+
+    private void EstablishSingleton()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void SpawnManagers()
+    {
+        //Spawns all prefabs in the _allManagers list
+        foreach (GameObject m in _allManagers)
+        {
+            Instantiate(m, transform.position, transform.rotation).transform.parent = gameObject.transform;
+        }
+    }
+
+    void AssignManagers()
+    {
+        Input = FindObjectOfType<InputManager>();
+        Save = FindObjectOfType<SaveManager>();
+        Scene = FindObjectOfType<SceneLoadingManager>();
+        VFX = FindObjectOfType<VFXManager>();
+    }
+}
