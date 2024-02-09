845c4ec11fcb09e115c407558a9a8dbc3c5b4055
cimmer1 <cimmer@mail.bradley.edu>
2024-02-08 18:39:23 -0600

    Spawning Objects

--- Assets/Scripts/SpawningObjects.cs
+++ Assets/Scripts/SpawningObjects.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class SpawningObjects : MonoBehaviour
 {
     [SerializeField] List<GameObject> Spawnables = new List<GameObject>();
+    [SerializeField] List<Transform> SpawnPoints = new List<Transform>();
     
     private void ObjectsSpawn()
     {
-        Instantiate(Spawnables[0], new Vector2(-7, 3), Quaternion.identity);
+        for (int i = 0; i < Spawnables.Count; i++)
+        {
+            //Random.Range(i, Spawnables.Count);
+            GameObject gameObject = Spawnables[Random.Range(0, Spawnables.Count)];
+            //Instantiate(Spawnables[Random.Range(i, Spawnables.Count)], SpawnPoints[i].position, Quaternion.identity);
+            Instantiate(gameObject, SpawnPoints[i].position, Quaternion.identity);
+        }
     }
+
+  
 
     private void Start()
     {
