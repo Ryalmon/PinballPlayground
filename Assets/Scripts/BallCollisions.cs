c1990de67d84185659af4e1f990fe0b61674235f
QMerrims <mmerrims@mail.bradley.edu>
2024-02-09 15:52:22 -0600

    NonFunctionalMovingObjectsKillbox

--- Assets/Scripts/BallCollisions.cs
+++ Assets/Scripts/BallCollisions.cs
@@ -31,7 +31,6 @@ public class BallKillBox : MonoBehaviour
             Destroy(gameObject);
             BallController.GetComponent<BallSpawner>().SetButtonActive();
             
-            
         }
 
         if(collision.gameObject.CompareTag("Splitter"))
--- Assets/Scripts/Gameplay/Drift.cs
+++ Assets/Scripts/Gameplay/Drift.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Drift : MonoBehaviour
 {
     [SerializeField] float DriftSpeed;
-    // Start is called before the first frame update
+    /// <summary>
+    /// Gives the placeable objects a natural upward drift
+    /// will probably need to be make less smooth/perfect. It visually feels very unnatural. 
+    /// </summary>
     void Start()
     {
         StartCoroutine(NaturalDrift());
@@ -18,5 +21,17 @@ public class Drift : MonoBehaviour
             yield return null;
         }
 
+    }
+    /// <summary>
+    /// This is supposed to destroy the drifting objects when they hit the killbox above the game
+    /// Unfortunately it doesn't work. 
+    /// </summary>
+    /// <param name="collision"></param>
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("KillBox"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
