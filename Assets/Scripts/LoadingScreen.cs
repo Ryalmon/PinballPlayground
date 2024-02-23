14141350fabb4860de35cbadd82c6a6337fdbd49
Smash-Sonic <smashsonic21@gmail.com>
2024-02-22 19:04:03 -0600

    Loading Screen transition code

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public GameObject loadScreen;
+
+    public void loadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        loadScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
