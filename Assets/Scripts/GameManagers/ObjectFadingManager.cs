62e7fdb7c6ce8ed6a05f91d754845e26f3cffc9f
Smash-Sonic <smashsonic21@gmail.com>
2024-03-29 13:34:06 -0500

    Quick Black Hole Fix

--- Assets/Scripts/GameManagers/ObjectFadingManager.cs
+++ Assets/Scripts/GameManagers/ObjectFadingManager.cs
@@ -44,10 +44,10 @@ public class ObjectFadingManager : MonoBehaviour
     {
         if (fadeObj == null) return;
         //Color newColor = fadeObj.GetComponent<SpriteRenderer>().material.color;
-        Color newColor = fadeObj.GetComponent<SpriteRenderer>().color;
+        Color newColor = fadeObj.GetComponentInChildren<SpriteRenderer>().color;
         newColor = new Color(newColor.r, newColor.g, newColor.b, newAlpha);
         //fadeObj.GetComponent<SpriteRenderer>().material.color = newColor;
-        fadeObj.GetComponent<SpriteRenderer>().color = newColor;
+        fadeObj.GetComponentInChildren<SpriteRenderer>().color = newColor;
     }
 
     public void StopSpecifiedCoroutine(Coroutine toStop)
