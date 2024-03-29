723aced13cacab7691e3fa70f3ec82e1747e9195
QMerrims <mmerrims@mail.bradley.edu>
2024-03-29 15:09:03 -0500

    BallRedirectorFix
    Ball redirector now works. It can't get the ball stuck anymore.

--- Assets/Scripts/Gameplay/BallRedirector.cs
+++ Assets/Scripts/Gameplay/BallRedirector.cs
@@ -6,13 +6,28 @@ public class BallRedirector : MonoBehaviour
 {
     [SerializeField] Vector2 _redirectDirection;
     [SerializeField] float _redirectForce;
+    private GameObject ball;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         BallPhysics ballphysics = collision.gameObject.GetComponent<BallPhysics>();
         if (ballphysics != null)
         {
-            ballphysics.OverrideBallForce(_redirectDirection * _redirectForce);
+            ball = collision.gameObject;
+            StartCoroutine(RedirectBall());
         }
+    }
+
+    private IEnumerator RedirectBall()
+    {
+        ball.transform.position = transform.position;
+        ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        ball.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        
+        yield return new WaitForSeconds(0.5f);
+        ball.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        ball.GetComponent<BallPhysics>().OverrideBallForce(_redirectDirection * _redirectForce);
+        
+
     }
 }
