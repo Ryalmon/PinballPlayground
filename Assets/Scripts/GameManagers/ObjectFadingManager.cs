50f2fd880dd8194e04d1b77d82de72778dcae77d
rswansonbradley <raswanson@mail.bradley.edu>
2024-03-31 23:17:34 -0500

    FadeTrail

--- Assets/Scripts/GameManagers/ObjectFadingManager.cs
+++ Assets/Scripts/GameManagers/ObjectFadingManager.cs
@@ -86,7 +86,7 @@ public class ObjectFadingManager : MonoBehaviour
 
     public void StartTrailFadeOut(GameObject fadeObj, float timeToFade)
     {
-        StartCoroutine(FadeTrail(fadeObj, 1, 0, timeToFade, null));
+        StartCoroutine(FadeTrail(fadeObj, fadeObj.GetComponent<TrailRenderer>().time, 0, timeToFade, null));
     }
     private IEnumerator FadeTrail(GameObject fadeObj, float startA, float endA, float timeToFade, UnityEvent postFade)
     {
@@ -97,22 +97,36 @@ public class ObjectFadingManager : MonoBehaviour
         {
             fadeProcessTimer += Time.deltaTime / timeToFade;
             currentAlpha = Mathf.Lerp(startA, endA, fadeProcessTimer);
-            ChangeTrailAlpha(fadeObj, currentAlpha);
+            ChangeTrailLength(fadeObj, currentAlpha);
+            //ChangeTrailAlpha(fadeObj, currentAlpha);
             yield return null;
         }
+        fadeObj.SetActive(false);
         if (postFade != null)
             postFade.Invoke();
+    }
+
+    private void ChangeTrailLength(GameObject fadeObj, float newLength)
+    {
+        if (fadeObj == null) return;
+        //Color newColor = fadeObj.GetComponent<SpriteRenderer>().material.color;
+        //fadeObj.GetComponent<TrailRenderer>().time;
+        fadeObj.GetComponent<TrailRenderer>().time = newLength;
+        
+        //fadeObj.GetComponent<TrailRenderer>().colorGradient.co
+        //Debug.Log(fadeObj.GetComponent<TrailRenderer>().material.color);
     }
 
     private void ChangeTrailAlpha(GameObject fadeObj, float newAlpha)
     {
         if (fadeObj == null) return;
         //Color newColor = fadeObj.GetComponent<SpriteRenderer>().material.color;
-        Color newColor = fadeObj.GetComponent<SpriteRenderer>().color;
+        Color newColor = fadeObj.GetComponent<TrailRenderer>().material.color;
         newColor = new Color(newColor.r, newColor.g, newColor.b, newAlpha);
         //fadeObj.GetComponent<SpriteRenderer>().material.color = newColor;
         fadeObj.GetComponent<TrailRenderer>().material.color = newColor;
         //fadeObj.GetComponent<TrailRenderer>().colorGradient.co
+        Debug.Log(fadeObj.GetComponent<TrailRenderer>().material.color);
     }
 }
 
