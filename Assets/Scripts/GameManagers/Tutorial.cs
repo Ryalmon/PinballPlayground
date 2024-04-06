229760fe02a92d1054bd36fc7138bc2d3670568f
JhonTravoltage <jeddy@mail.bradley.edu>
2024-04-06 11:36:50 -0500

    Testing the tutorial

---
+++ Assets/Scripts/GameManagers/Tutorial.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject _placementField;
+    [SerializeField] private GameObject _ballLauncher;
+
+    [SerializeField] private SpriteRenderer _backgroundImage;
+    [SerializeField] private SpriteRenderer _borderImage;
+
+    private Color _darken = new Color(0.45f, 0.45f, 0.45f);
+    private Color _normal = new Color(1, 1, 1);
+
+    private bool _tutorialActive = false;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TutorialPopUp());
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _tutorialActive = false;
+
+            Time.timeScale = 1f;
+
+            _placementField.SetActive(false);
+            _ballLauncher.SetActive(true);
+
+            _backgroundImage.color = _normal;
+            _borderImage.color = _normal;
+        }
+    }
+
+    private IEnumerator TutorialPopUp()
+    {
+        yield return new WaitForSeconds(0.8f);
+
+        _tutorialActive = true;
+
+        Time.timeScale = 0f;
+
+        _placementField.SetActive(true);
+        _ballLauncher.SetActive(false);
+
+        _backgroundImage.color = _darken;
+        _borderImage.color = _darken;
+    }
+}
